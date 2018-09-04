6057548d854f65694e9c1f03d7fe4de77f799f0f
berkantbaba <41677093+berkantbaba@users.noreply.github.com>
2018-09-04 10:19:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 // -->COMMANDS<--
 
 bot.on('guildMemberAdd',member =>{
-    member.send('** :trident: Lonely Kingdom ve Fearless Suncusuna Dvetlsiniz:trident: PROJESI**  https://discord.gg/NwYf85s https://discord.gg/zruSq2h\n**COVER**https://gph.is/2vXgvnt');
+    member.send('** :trident: Lonely Kingdom  Suncusuna Dvetlsiniz:trident: **PROJESI** https://discord.gg/zruSq2h\n**COVER**https://gph.is/2vXgvnt');
 
     //member.guild.defaultChannel.sendmessage(`Welcome "${member.user.username}"! Be sure to set your platform by typing "!role"`);
 
